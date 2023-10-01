50eb5e2b52cb351a8f2d392dd9db366f790f65d0
Monil Prajapati <monillprajaptii@gmail.com>
2023-10-01 12:57:26 +0530

    Project Initialized

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-6xl text-center'>Project Newsify : Version 1.0.0</h1>
+    </div>
+  )
+}
+
+export default App
